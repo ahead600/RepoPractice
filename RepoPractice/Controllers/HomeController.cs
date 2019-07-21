afb3c720bdcf73f8163a5d740440101752e5c947
Eslami <m2eslami@gmail.com>
2019-07-21 14:10:52 +0430

    Commit Controller

--- RepoPractice/Controllers/HomeController.cs
+++ RepoPractice/Controllers/HomeController.cs
@@ -10,9 +10,16 @@ namespace RepoPractice.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UnitOfWork _unitOfWork;
+
+        public HomeController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var model = _unitOfWork.Courses.GetAll();
+            return View(model);
         }
 
         public IActionResult Privacy()
